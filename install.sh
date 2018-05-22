9fa198c3d89279fb0878d12d269a59153ebb6c0b
ShadowStar <orphen.leiliu@gmail.com>
2018-05-22 19:53:43 +0800

    Feature (Install Script): New install script for simple installation

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: install.sh
+# 
+#         USAGE: ./install.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: ShadowStar, <orphen.leiliu@gmail.com>
+#  ORGANIZATION: Gmail
+#       CREATED: 05/22/2018 19:32:35
+#   LAST CHANGE:05/22/2018 19:53:20
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+DIR="$(dirname "$(realpath $0)")"
+
+ln -sv ${DIR}/bash_profile ${HOME}/.bash_profile
+ln -sv ${DIR}/bashrc ${HOME}/.bashrc
+ln -sv ${DIR}/zshrc ${HOME}/.zshrc
+ln -sv ${DIR}/ctags_global ${HOME}/.ctags
+ln -sv ${DIR}/gitconfig_global ${HOME}/.gitconfig
+ln -sv ${DIR}/gitignore_global ${HOME}/.gitignore_global
+ln -sv ${DIR}/gitattributes_global ${HOME}/.gitattributes_global
+ln -sv ${DIR}/gitcommit_template ${HOME}/.gitcommit_template
+
