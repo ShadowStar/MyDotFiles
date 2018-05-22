992acfbeb6b7ce069f9d2aaedd8a223cd71ed52f
ShadowStar <orphen.leiliu@gmail.com>
2018-05-22 21:30:25 +0800

    Feature (Install Script): Support link to broken link-files

--- install.sh
+++ install.sh
@@ -11,10 +11,10 @@
 #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
-#        AUTHOR: ShadowStar, <orphen.leiliu@gmail.com>
+#        AUTHOR: 
 #  ORGANIZATION: Gmail
 #       CREATED: 05/22/2018 19:32:35
-#   LAST CHANGE:05/22/2018 20:17:28
+#   LAST CHANGE:05/22/2018 21:26:32
 #      REVISION:  ---
 #===============================================================================
 
@@ -26,12 +26,17 @@ if [ "${HOME}" == "$(dirname "${DIR}")" ]; then
 	DIR="$(basename ${DIR})"
 fi
 
-ln -sv ${DIR}/bash_profile ${HOME}/.bash_profile
-ln -sv ${DIR}/bashrc ${HOME}/.bashrc
-ln -sv ${DIR}/zshrc ${HOME}/.zshrc
-ln -sv ${DIR}/ctags_global ${HOME}/.ctags
-ln -sv ${DIR}/gitconfig_global ${HOME}/.gitconfig
-ln -sv ${DIR}/gitignore_global ${HOME}/.gitignore_global
-ln -sv ${DIR}/gitattributes_global ${HOME}/.gitattributes_global
-ln -sv ${DIR}/gitcommit_template ${HOME}/.gitcommit_template
+function linkfile ()
+{
+	[ ! -e ${2} ] && ln -sfv ${1} ${2} || echo "${2} existed"
+}
+
+linkfile ${DIR}/bash_profile ${HOME}/.bash_profile
+linkfile ${DIR}/bashrc ${HOME}/.bashrc
+linkfile ${DIR}/zshrc ${HOME}/.zshrc
+linkfile ${DIR}/ctags_global ${HOME}/.ctags
+linkfile ${DIR}/gitconfig_global ${HOME}/.gitconfig
+linkfile ${DIR}/gitignore_global ${HOME}/.gitignore_global
+linkfile ${DIR}/gitattributes_global ${HOME}/.gitattributes_global
+linkfile ${DIR}/gitcommit_template ${HOME}/.gitcommit_template
 
